b95b1d3e1f2b5690e9ca7d6a0217fedcb6bccdfc
CamilaGL18 <cl2317916@gmail.com>
2024-09-04 16:07:34 -0300

    area

---
+++ atv03.py
@@ -0,0 +1,7 @@
+# Crie um programa que calcule a área de um retângulo, pedindo ao usuário a largura e a altura.
+valor1= int(input("colocar o valor da largura"))
+valor2= int(input("colocar o valor da altura"))
+
+area= valor1*valor2
+
+print (f"a area do retangulo e ={area}")
